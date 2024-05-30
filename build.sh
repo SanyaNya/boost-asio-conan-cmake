65c64a35650ce346cf2139eb31f53dcfc95eaacd
SanyaNya <sanyanya4ever@gmail.com>
2024-05-30 15:50:34 +0300

    Add custom linux profiles with its own build directories

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-conan install . --build=missing
-cmake --preset conan-release
-cmake --build --preset conan-release
+conan install . --build=missing --profile=profiles/conan-$1 \
+  -c tools.cmake.cmake_layout:build_folder_vars="['settings.os', 'settings.arch', 'settings.compiler', 'settings.build_type']"
+cmake --preset conan-$1
+cmake --build --preset conan-$1
